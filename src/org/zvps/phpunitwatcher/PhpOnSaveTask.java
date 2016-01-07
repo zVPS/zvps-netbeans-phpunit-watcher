86a4a01dff47692663e7d9e321bec51527dc3a2a
Kevin Andrews <kevin@zvps.uk>
2016-01-07 11:04:46 +0000

    Updated with a contributions list and copyright year.

--- src/org/zvps/phpunitwatcher/PhpOnSaveTask.java
+++ src/org/zvps/phpunitwatcher/PhpOnSaveTask.java
@@ -1,6 +1,6 @@
 /**
  *   PHPUnit Watcher - run unit tests on save
- *   Copyright (C) 2014  nForced Website Hosting Limited
+ *   Copyright (C) 2016  nForced Website Hosting Limited
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -14,6 +14,12 @@
  *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *   Contributions List (please see github.com/zVPS/zvps-netbeans-phpunit-watcher)
+ *   for an up-to-date list
+ *   https://github.com/tmysik
+ *   https://github.com/Caffe1neAdd1ct
+ * 
  **/
 package org.zvps.phpunitwatcher;
 
