a07a683fd3ddd0504d7b8ca1f23d4b7bf9d2f5a2
Kevin Andrews <kevin@zvps.uk>
2016-02-19 12:36:01 +0000

    Splitting out Runnable code and on save hook/ Signed-off-by:Kevin Andrews <kevin@zvps.uk>

--- src/org/zvps/phpunitwatcher/PhpOnSaveTask.java
+++ src/org/zvps/phpunitwatcher/PhpOnSaveTask.java
@@ -23,29 +23,23 @@
  **/
 package org.zvps.phpunitwatcher;
 
-import java.awt.EventQueue;
-import java.util.Arrays;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
-import org.netbeans.api.project.FileOwnerQuery;
-import org.netbeans.api.project.Project;
-import org.netbeans.api.project.ProjectUtils;
-import org.netbeans.api.project.SourceGroup;
 import org.netbeans.modules.editor.NbEditorUtilities;
 import org.netbeans.spi.editor.document.OnSaveTask;
-import org.netbeans.spi.project.ActionProvider;
 import org.openide.filesystems.FileObject;
-import org.openide.filesystems.FileUtil;
-import org.openide.util.Lookup;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
 import org.openide.util.RequestProcessor;
-import org.openide.util.lookup.Lookups;
+import org.openide.util.TaskListener;
 
-public final class PhpOnSaveTask implements OnSaveTask, Runnable {
-
-    private static final RequestProcessor RP = new RequestProcessor(PhpOnSaveTask.class);
-
-    private final FileObject fileObject;
-
-
+public final class PhpOnSaveTask implements OnSaveTask {
+    
+    private final static RequestProcessor RP = new RequestProcessor("phpunitwatchertasks", 1, true);
+    
+    private FileObject fileObject;
+    
     PhpOnSaveTask(FileObject fileObject) {
         assert fileObject != null;
         this.fileObject = fileObject;
@@ -53,66 +47,35 @@ public final class PhpOnSaveTask implements OnSaveTask, Runnable {
 
     @Override
     public void performTask() {
-        // noop
     }
 
     @Override
     public void runLocked(Runnable run) {
-        RP.post(this);
+        
+        PhpUnitTestRunnable UnitTestRunnable = new PhpUnitTestRunnable(fileObject);
+        
+        final RequestProcessor.Task theTask = RP.create(UnitTestRunnable);
+        
+        final ProgressHandle ph = ProgressHandle.createHandle("performing some task", theTask);
+        theTask.addTaskListener(new TaskListener() {
+            @Override
+            public void taskFinished(org.openide.util.Task task) {
+                //make sure that we get rid of the ProgressHandle
+                //when the task is finished
+                ph.finish();
+            }
+        });
+        
+        theTask.schedule(1000);
     }
 
     @Override
     public boolean cancel() {
         // noop
-        return false;
-    }
-
-    @Override
-    public void run() {
-        Project project = FileOwnerQuery.getOwner(fileObject);
-        if (project == null) {
-            return;
-        }
-        SourceGroup[] sourceGroups = ProjectUtils.getSources(project).getSourceGroups("PHPSOURCE"); // NOI18N
-        if (sourceGroups.length < 1) {
-            return;
-        }
-        if (!isSourceFile(fileObject, sourceGroups)) {
-            return;
-        }
-        final ActionProvider actionProvider = project.getLookup().lookup(ActionProvider.class);
-        if (actionProvider == null) {
-            return;
-        }
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Lookup lookup = Lookups.fixed(fileObject);
-                if (Arrays.asList(actionProvider.getSupportedActions()).contains(ActionProvider.COMMAND_TEST_SINGLE)
-                        && actionProvider.isActionEnabled(ActionProvider.COMMAND_TEST_SINGLE, lookup)) {
-                    actionProvider.invokeAction(ActionProvider.COMMAND_TEST_SINGLE, lookup);
-                }
-            }
-        });
-    }
-
-    // a it hack to avoid impl dep on php.api.phpmodule
-    private static boolean isSourceFile(FileObject fileObject, SourceGroup[] sourceGroups) {
-        if (!FileUtil.isParentOf(sourceGroups[0].getRootFolder(), fileObject)) {
-            // not a source file
-            return false;
-        }
-        for (int i = 1; i < sourceGroups.length; ++i) {
-            if (FileUtil.isParentOf(sourceGroups[i].getRootFolder(), fileObject)) {
-                // some test file
-                return false;
-            }
-        }
         return true;
     }
-
+    
     //~ Factories
-
     @MimeRegistration(mimeType = "text/x-php5", service = OnSaveTask.Factory.class, position = 2000)
     public static final class Factory implements OnSaveTask.Factory {
 
---
+++ src/org/zvps/phpunitwatcher/PhpUnitTestRunnable.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zvps.phpunitwatcher;
+
+import java.awt.EventQueue;
+import java.util.Arrays;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.spi.project.ActionProvider;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Lookup;
+import org.openide.util.RequestProcessor;
+import org.openide.util.lookup.Lookups;
+
+/**
+ *
+ * @author kandrews
+ */
+public class PhpUnitTestRunnable implements Runnable {
+    
+    private final FileObject fileObject;
+    
+    PhpUnitTestRunnable(FileObject fileObject) {
+        this.fileObject = fileObject;
+    }
+    
+    public void run() {
+        Project project = FileOwnerQuery.getOwner(fileObject);
+        if (project == null) {
+            return;
+        }
+        SourceGroup[] sourceGroups = ProjectUtils.getSources(project).getSourceGroups("PHPSOURCE"); // NOI18N
+        if (sourceGroups.length < 1) {
+            return;
+        }
+        if (!isSourceFile(fileObject, sourceGroups)) {
+            return;
+        }
+        final ActionProvider actionProvider = project.getLookup().lookup(ActionProvider.class);
+        if (actionProvider == null) {
+            return;
+        }
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Lookup lookup = Lookups.fixed(fileObject);
+                if (Arrays.asList(actionProvider.getSupportedActions()).contains(ActionProvider.COMMAND_TEST_SINGLE)
+                        && actionProvider.isActionEnabled(ActionProvider.COMMAND_TEST_SINGLE, lookup)) {
+                    actionProvider.invokeAction(ActionProvider.COMMAND_TEST_SINGLE, lookup);
+                }
+            }
+        });
+    }
+
+    // a it hack to avoid impl dep on php.api.phpmodule
+    private static boolean isSourceFile(FileObject fileObject, SourceGroup[] sourceGroups) {
+        if (!FileUtil.isParentOf(sourceGroups[0].getRootFolder(), fileObject)) {
+            // not a source file
+            return false;
+        }
+        for (int i = 1; i < sourceGroups.length; ++i) {
+            if (FileUtil.isParentOf(sourceGroups[i].getRootFolder(), fileObject)) {
+                // some test file
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
